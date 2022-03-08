78dbb71fe2d8ed7321ba7cff819b36e6bf6d1a77
septa97 <jancharles.adona@gmail.com>
2022-03-08 10:57:53 +0800

    Add day 2 solutions (naive) and input

---
+++ day2/main.rb
@@ -0,0 +1,44 @@
+lines = File.readlines('day2/input.txt')
+
+def part1(lines)
+  depth = 0
+  horizontal = 0
+
+  lines.each do |line|
+    ins, arg = line.split
+    arg = arg.to_i
+    if ins == 'forward'
+        horizontal += arg
+    elsif ins == 'down'
+        depth += arg
+    else # we're sure that it's "up"
+        depth -= arg
+    end
+  end
+
+  horizontal * depth
+end
+
+def part2(lines)
+  depth = 0
+  horizontal = 0
+  aim = 0
+
+  lines.each do |line|
+    ins, arg = line.split
+    arg = arg.to_i
+    if ins == 'down'
+        aim += arg
+    elsif ins == 'up'
+        aim -= arg
+    else # we're sure that it's "forward"
+        horizontal += arg
+        depth += (aim * arg)
+    end
+  end
+
+  horizontal * depth
+end
+
+puts "part 1: #{part1(lines)}"
+puts "part 2: #{part2(lines)}"
